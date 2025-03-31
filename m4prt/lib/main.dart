bd6d0afaa553114553c70acd3448f736ead01c7d
Sujan Sahu <153996083+sujan759@users.noreply.github.com>
2025-03-31 07:58:28 +0530

    Initial commit

---
+++ m4prt/lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+// What is ListTile and CircleAvatar?
+// How to implement with ListView and ListView.builder
+void main(){
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true
+      ),
+      home: HomePage(),
+
+    );
+  }
+}
+
+class HomePage extends StatelessWidget{
+  List<Map<String, dynamic>> mChats =[
+    {
+      'Name': 'Sujan Sahu',
+      'Msg':'What are do!',
+      "time": "01:01 AM",
+      "UnreadCount": 1,
+      "profilePic":"https://stablediffusionweb.com/image/5282915-creative-cartoon-profile-picture"
+      },
+    {
+      'Name': 'Raj Sahu',
+      'Msg':'Hii!',
+      "time": "10:01 AM",
+      "UnreadCount": 0,
+      "profilePic":"https://stablediffusionweb.com/image/5282915-creative-cartoon-profile-picture"
+    },{
+      'Name': 'Ashis Sahu',
+      'Msg':'Hello are you free!',
+      "time": "08:01 AM",
+      "UnreadCount": 3,
+      "profilePic":"https://stablediffusionweb.com/image/5282915-creative-cartoon-profile-picture"
+    },{
+      'Name': 'Sam Sahu',
+      'Msg':'Good Morning!',
+      "time": "06:01 AM",
+      "UnreadCount": 2,
+      "profilePic":"https://stablediffusionweb.com/image/5282915-creative-cartoon-profile-picture"
+    },
+
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+
+      body: ListView.builder(
+        itemCount: mChats.length,
+          itemBuilder: (context,index){
+            return ListTile(
+              // leading: Text('${index+1}.'),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage('${mChats[index]['profilePic']}')
+              ),
+              title:Text('${mChats[index]['Name']}') ,
+              subtitle:Text('${mChats[index]['Msg']}') ,
+              // trailing: Text('${mChats[index]['time']}'),
+              trailing: Column(
+                children: [
+
+                  Text('${mChats[index]['time']}'),
+                  if(mChats[index]['UnreadCount']>0)
+                  CircleAvatar(
+                    maxRadius: 15,
+                    backgroundColor: Colors.green,
+                    foregroundColor: Colors.white,
+                    child: Text('${mChats[index]['UnreadCount']}'),
+                    
+                  )
+
+                  
+                ],
+              ),
+            );
+
+
+      })
+
+    );
+  }
+}

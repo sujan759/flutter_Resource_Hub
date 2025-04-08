435500d3c73d3c6ae3cc4b7550b9cbc1470c6fd1
Sujan <sujansahu655@gmail.com>
2025-04-08 20:04:13 +0530

    finally  complete  first ui

--- first_ui/lib/main.dart
+++ first_ui/lib/main.dart
@@ -1,122 +1,312 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget{
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'First UI',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatelessWidget{
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+        body: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+          Container(
+            height: 380,
+            width: 300,
+            color: Color(0xff00ff00),
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(3),
+
+                  height: 40,
+                width: 290,
+                color: Color(0xffe0dade),
+                  child: Text('Strawbery Pavolova',textAlign: TextAlign.center,style: TextStyle(
+                    fontSize: 25,
+                  ),),
+
+                ),
+
+                Container(
+                  margin: EdgeInsets.all(3),
+                  height: 130,
+                  width: 290,
+                  color: Color(0xffe0dade),
+                  child: Text('Strawberry Pavlova is a light, cloud-like dessert with a crisp meringue shell, fluffy marshmallow center, and fresh strawberries layered over whipped cream.',
+                    textAlign: TextAlign.justify,
+                    style: TextStyle(
+                    fontSize: 18,
+                  ),),
+
+                ),
+
+                Container(
+                  margin: EdgeInsets.all(3),
+                  height: 40,
+                  width: 290,
+                  color: Color(0xffe0dade),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 30),
+                        child: Container(
+
+                          child: Text("★★★★★",textAlign: TextAlign.center,),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.only(left: 40),
+                        child: Container(
+                          child: Text("170  Review",
+                          textAlign: TextAlign.center,),
+                        ),
+                      )
+
+
+                    ],
+                  ),
+
+                ),
+                SizedBox(height: 8),
+
+                Container(
+                  margin: EdgeInsets.all(3),
+                  height: 110,
+                  width: 290,
+                  color: Color(0xffe0dade),
+                  child: Row(
+                    children: [
+
+                      Padding(
+                        padding: const EdgeInsets.only(left: 50),
+                        child: Container(
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 15),
+                            child: Column(
+                              children: [
+                                Container(
+                                  height: 30,
+                                  width: 30,
+                                  child: Image.asset ('assets/images/icon_image/fridge_icon.png'),
+                                ),
+                                SizedBox(height: 5,),
+                                Container(
+                                  child: Text('Store',style: TextStyle(fontWeight: FontWeight.bold),),
+                                ),
+
+                                SizedBox(height: 15,),
+                                Container(
+                                  child: Text('25 mine',style: TextStyle(fontStyle: FontStyle.italic),),
+                                ),
+
+
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      
+                      SizedBox(width:20,),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Container(
+                          child: Padding(
+                            padding: const EdgeInsets.only(top: 15),
+                            child: Column(
+                              children: [
+                                Container(
+                                  height: 30,
+                                  width: 30,
+                                  child: Image.asset ('assets/images/icon_image/watch_icon.png'),
+                                ),
+                                SizedBox(height: 5,),
+                                Container(
+                                  child: Text('Clock',style: TextStyle(fontWeight: FontWeight.bold),),
+                                ),
+
+                                SizedBox(height: 15,),
+                                Container(
+                                  child: Text('1 hr',style: TextStyle(fontStyle: FontStyle.italic),),
+                                ),
+
+
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(width: 30,),
+                      Container(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 15),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 30,
+                                width: 30,
+                                // child: Image.asset ('assets/images/icon_image/food_icon.png'),
+                                child: Image.asset('assets/images/icon_image/food_icon.png'),
+                              ),
+                              SizedBox(height: 5,),
+                              Container(
+                                child: Text('Foods',style: TextStyle(fontWeight: FontWeight.bold),),
+                              ),
+
+                              SizedBox(height: 15,),
+                              Container(
+                                child: Text('1-3 ',style: TextStyle(fontStyle: FontStyle.italic),),
+                              ),
+
+
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                ),
+              ],
             ),
-          ],
-        ),
+
+          ),
+
+
+          Container(
+            margin: EdgeInsets.all(10),
+            height: 400,
+            width: 600,
+            child: Image.asset('assets/images/background_img1.png'),
+          ),
+
+
+
+
+        ],
+
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+// body:Padding (
+//   padding:  EdgeInsets.all(30.0),
+//
+//   child: Container(
+//     height: 600,
+//     width: 1500,
+//     color: Color(0xFFD3DCD3),
+//
+//       child: Row(
+//         children: [
+//
+//           Container(
+//             decoration: BoxDecoration(color: Color(0xffec0cec)),
+//           ),
+//           Column(
+//             children: [
+//               Container(
+//                 height: 500,
+//                 width:550 ,
+//                 color: Color(0xff6d91e7),
+//
+//               ),
+//
+//               Row(
+//                 children: [
+//                   Container(
+//                     height: 100,
+//                     width:100 ,
+//                     color: Color(0xff1ce7e0),
+//                   ),
+//                 ],
+//
+//               ),
+//
+//               Row(
+//                 children: [
+//                   Container(
+//                     height: 100,
+//                     width:100 ,
+//                     color: Color(0xffdde31e),
+//                   ),
+//                 ],
+//
+//               ),
+//
+//               Row(
+//                 children: [
+//                   Container(
+//                     height: 100,
+//                     width:100 ,
+//                     color: Color(0xff0e46d3),
+//                   ),
+//                 ],
+//
+//               ),
+//
+//               Row(
+//                 children: [
+//                   Container(
+//                     height: 100,
+//                     width:0100 ,
+//                     color: Color(0xff8131d7),
+//                   ),
+//                 ],
+//
+//               ),
+//             ],
+//           ),
+//
+//           Column(
+//             children: [
+//               Container(
+//                 margin: EdgeInsets.all(20),
+//                 height: 550,
+//                 width:886 ,
+//                 child: Image.asset('assets/images/background_img1.png', fit:BoxFit.fill,)
+//
+//                 // color: Color(0xffe1c9ca),
+//                 // decoration: BoxDecoration(
+//                 //   image: DecorationImage(
+//                 //
+//                 //     image: AssetImage('assets/images/background_img1.png'),
+//                 //     fit: BoxFit.cover,
+//                 //   ),
+//                 //   // borderRadius: BorderRadius.circular(10),
+//                 // ),
+//               )
+//
+//
+//             ],
+//
+//           )
+//         ],
+//       ),
+//     ),
+//
+// ),
